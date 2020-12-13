7bbb5e0ea8735a9a2c01ad904f11d2858a83b421
Matias Niemelä <matias@nieme.la>
2020-12-12 19:04:14 -0800

    remove travis testing for now

--- src/app/code-modal-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CodeModalClickDirective } from './code-modal-click.directive';
-
-describe('CodeModalClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CodeModalClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/tool-tip-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToolTipHoverDirective } from './tool-tip-hover.directive';
-
-describe('ToolTipHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToolTipHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
